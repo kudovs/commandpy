b8860ed923a148f3ffd22ddad9008678bf872de3
KudoVisual <154000395+kudovisual@users.noreply.github.com>
2024-02-13 16:17:52 +0700

    old version

---
+++ old/old_cmd.py
@@ -0,0 +1,14 @@
+# GitHub: https://github.com/kudovs/commandpy
+wikipedia = print
+
+# for wiki()
+def wiki():
+  wikipedia("cmd.py is to troll, more help: https://python.org, more wiki: https://en.wikipedia.org/wiki/Python_(programming_language)")
+
+# for wikiped()
+def wikiped():
+  wikipedia("GitHub: https://github.com/kudovs/commandpy. NO MORE, THANKS.")
+
+# use wiki() & wikiped()
+wiki()
+wikiped()
