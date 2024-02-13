2229943e32a5466d45dd82eff79ba5b7bc6436c5
KudoVisual <154000395+kudovisual@users.noreply.github.com>
2024-02-13 16:10:29 +0700

    new version

---
+++ cmd.py
@@ -0,0 +1,18 @@
+
+# Official CMD.PY (to troll)
+# import kudovisual
+
+wikipedia = print
+
+def cmdpy_run():
+    wikipedia("Python is a programming language. Not is a Terminal app, not a Command Prompt.")
+
+
+def wikipedia_print():
+    wikipedia("Please search Python in Wikipedia, or visit https://en.wikipedia.org/wiki/Python_(programming_language) for help.")
+
+
+wikipedia("Go file wikipedia.bat (windows) to visit https://en.wikipedia.org/wiki/Python_(programming_language).")
+
+wikipedia_print()
+cmdpy_run()
